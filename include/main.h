3ac2d4470cc59a42763141c90ff65f1a5d78df12
phucvinh57 <npvinh0507@gmail.com>
2023-02-12 22:17:51 +0700

    handle exception case

--- include/main.h
+++ include/main.h
@@ -1,6 +1,12 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
-#include "process.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <climits>
 
 #endif
