4ef21587fe73010c82b5137d90696bccd6149219
nhancdt <nhancdt@vng.com.vn>
2023-03-27 19:29:33 +0700

    add example of setup websocket client

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,27 @@
-#include "sensor.h"
+#include "ws_manager_client.h"
+#include "handlers/auth_handler.h"
+#include "handlers/sys_info_handler.h"
+#include "sensor_logger.h"
+#include "iostream"
+#include "process.h"
 
+void setupWsClient() {
+    SensorManagerClient *client = SensorManagerClient::GetInstance();
+    auto *authHandler = new AuthHandler();
+    auto *sysInfoHandler = new SysInfoHandler();
+
+    client->registerHandler(WsCommand::AUTH, authHandler);
+    client->registerHandler(WsCommand::SYS_INFO, sysInfoHandler);
+
+    client->setupAndStart();
+}
 int main(int argc, char *argv[])
 {
-    ProcessInfo p(1);
-    printProcessInfo(&p);
+//    ProcessInfo p(1);
+//    printProcessInfo(&p);
+// Uncomment this line to setup ws client
+    setupWsClient();
+
 
     return 0;
 }
